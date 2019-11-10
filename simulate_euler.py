066f7cf1f6b1eb52803498123b7ba20b0de35839
Hagai Kariti <hkariti@gmail.com>
2019-11-10 09:47:14 +0200

    add some reusable items
    * FitzHugh-Nagumo model as a reusable func
    * FH-N stable point
    * generic function to count spikes

--- simulate_euler.py
+++ simulate_euler.py
@@ -9,3 +9,16 @@ def simulate(f, x0, dt, Tmax):
         dx = f(x[t, :])
         x[t+1, :] = dt * dx + x[t, :]
     return x
+
+def fitzhugh_nagumo_dx(x, I=0, a=0.7, b=0.8, epsilon=0.01):
+    return np.array([x[0] - x[0]**3/3 - x[1] + I, epsilon*(x[0] + a - b*x[1])])
+
+fitzhugh_nagumo_stable_point = (-1.2, -0.62)
+
+def count_spikes(x, A=1.5):
+    mask = x > A
+    diff = mask[1:] > mask[:-1]
+    spikes = diff.sum()
+    if spikes > 0:
+        return spikes - 1
+    return 0
